21add549f318cc83076d5c9671a933ad19fc7031
unknown <kanibalaji16@gmail.com>
2023-06-25 15:04:58 +0530

    initial commit

---
+++ NaturalNumbers.java
@@ -0,0 +1,17 @@
+package org.naturalnumbers;
+
+public class NaturalNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=100,i,sum=0;
+		for(i=1;i<=100;i++)
+		{
+			sum=sum+i;
+			System.out.println("Sum of n natural number is"+sum);
+			
+		}
+
+	}
+
+}
